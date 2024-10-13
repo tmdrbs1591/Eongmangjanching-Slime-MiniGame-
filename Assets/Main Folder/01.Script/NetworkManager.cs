4627b5eed837c9f07b3460a0ba76470bc1d2c2a6
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-10-14 00:07:17 +0900

    추가 | 방 코드로 참가

--- Assets/Main Folder/01.Script/NetworkManager.cs
+++ Assets/Main Folder/01.Script/NetworkManager.cs
@@ -32,26 +32,32 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public TMP_Text StatusText;
     public PhotonView PV;
 
+    [Header("Room Code")]
+    public TMP_InputField RoomCodeInput; // 방 코드를 입력받을 필드 추가
+    public TMP_Text RoomCodeText; // 생성된 방 코드를 표시할 텍스트
+
     List<RoomInfo> myList = new List<RoomInfo>();
     int currentPage = 1, maxPage, multiple;
 
     [Header("LobbyPanel")]
-    public GameObject StartButton; // 시작 버튼 UI를 연결합니다.
+    public GameObject StartButton;
+
+    string roomCode; // 방 코드를 저장할 변수
 
     void Awake()
     {
         Screen.SetResolution(960, 540, false);
-        // 씬 자동 동기화 설정
         PhotonNetwork.AutomaticallySyncScene = true;
     }
+
     public void Start()
     {
-        // 방장이 아니면 시작 버튼을 비활성화합니다.
         if (!PhotonNetwork.IsMasterClient)
         {
             StartButton.SetActive(false);
         }
     }
+
     void Update()
     {
         StatusText.text = PhotonNetwork.NetworkClientState.ToString();
@@ -62,11 +68,11 @@ public class NetworkManager : MonoBehaviourPunCallbacks
             Send();
         }
     }
+
     public void StartGame()
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            // 방에 있는 모든 플레이어가 SampleScene 씬으로 이동
             PhotonNetwork.LoadLevel("SampleScene");
         }
     }
@@ -114,7 +120,6 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     #region 서버연결
 
-
     public void Connect() => PhotonNetwork.ConnectUsingSettings();
 
     public override void OnConnectedToMaster() => PhotonNetwork.JoinLobby();
@@ -136,39 +141,81 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        LobbyPanel.SetActive(true);  // 로비 패널 활성화
-        RoomPanel.SetActive(false);  // 방 패널 비활성화
-        DisconnectPanel.SetActive(true); // 필요한 경우 Disconnect 패널 활성화
+        LobbyPanel.SetActive(true);
+        RoomPanel.SetActive(false);
+        DisconnectPanel.SetActive(true);
     }
     #endregion
 
-    #region 방
-    public void CreateRoom() => PhotonNetwork.CreateRoom(RoomInput.text == "" ? "Room" + Random.Range(0, 100) : RoomInput.text, new RoomOptions { MaxPlayers = 4 });
+    #region 방 생성 및 참가
 
-    public void JoinRandomRoom() => PhotonNetwork.JoinRandomRoom();
+    // 방 이름에 방 코드를 포함하는 방식으로 생성
+    public void CreateRoom()
+    {
+        roomCode = Random.Range(1000, 9999).ToString(); // 4자리 방 코드 생성
+        string roomName = (RoomInput.text == "" ? "Room" + Random.Range(0, 100) : RoomInput.text) + "_" + roomCode; // 방 이름에 방 코드 추가
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+        PhotonNetwork.CreateRoom(roomName, roomOptions);
+        RoomCodeText.text = "Room Code: " + roomCode; // UI에 방 코드 표시
+    }
 
-    public void LeaveRoom() => PhotonNetwork.LeaveRoom();
+    // 사용자가 방 코드 입력 후 참가
+    public void JoinRoomWithCode()
+    {
+        string inputCode = RoomCodeInput.text;
+
+        // 방 목록에서 방 코드로 필터링하여 입장
+        foreach (RoomInfo roomInfo in myList)
+        {
+            if (roomInfo.Name.EndsWith("_" + inputCode))
+            {
+                PhotonNetwork.JoinRoom(roomInfo.Name);
+                return;
+            }
+        }
+
+        // 방을 찾지 못하면 에러 메시지를 띄울 수 있음
+        Debug.LogError("Room with code " + inputCode + " not found.");
+    }
+
+    // 랜덤 방 참가 기능
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
 
     public override void OnJoinedRoom()
     {
-        // 방에 들어갔을 때 버튼 상태를 확인합니다.
         RoomPanel.SetActive(true);
         RoomRenewal();
         ChatInput.text = "";
         for (int i = 0; i < ChatText.Length; i++) ChatText[i].text = "";
 
-        // 방장이 아니라면 시작 버튼을 비활성화
         StartButton.SetActive(PhotonNetwork.IsMasterClient);
 
+        // 방 코드 표시: 방 코드만 추출하여 UI에 표시
+        string roomName = PhotonNetwork.CurrentRoom.Name;
+        string roomCode = roomName.Split('_')[^1];  // 방 이름에서 마지막 부분이 방 코드
+        RoomCodeText.text = "Room Code: " + roomCode;
 
         LobbyPanel.SetActive(false);
         DisconnectPanel.SetActive(false);
         PhotonNetwork.Instantiate("Player", new Vector3(0, 10, 0), Quaternion.identity);
     }
 
-    public override void OnCreateRoomFailed(short returnCode, string message) { RoomInput.text = ""; CreateRoom(); }
 
-    public override void OnJoinRandomFailed(short returnCode, string message) { RoomInput.text = ""; CreateRoom(); }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        RoomInput.text = "";
+        CreateRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        RoomInput.text = "";
+        CreateRoom();
+    }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
@@ -181,7 +228,6 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         RoomRenewal();
         ChatRPC("<color=yellow>" + otherPlayer.NickName + "님이 퇴장하셨습니다</color>");
 
-        // 방장이 나가면 자동으로 방장이 바뀌므로, 새 방장이 시작 버튼을 활성화할 수 있게 업데이트합니다.
         if (PhotonNetwork.IsMasterClient)
         {
             StartButton.SetActive(true);
@@ -195,8 +241,6 @@ public class NetworkManager : MonoBehaviourPunCallbacks
             ListText.text += PhotonNetwork.PlayerList[i].NickName + ((i + 1 == PhotonNetwork.PlayerList.Length) ? "" : ", ");
         RoomInfoText.text = PhotonNetwork.CurrentRoom.Name + " / " + PhotonNetwork.CurrentRoom.PlayerCount + "명 / " + PhotonNetwork.CurrentRoom.MaxPlayers + "최대";
     }
-
-
     #endregion
 
     #region 채팅
@@ -225,3 +269,4 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     }
     #endregion
 }
+
