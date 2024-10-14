afc577355510a8d509d09e75a0c1ed685a4286a4
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-10-14 17:40:48 +0900

    추가 | 점수 동기화

--- Assets/Main Folder/01.Script/PlayerScore.cs
+++ Assets/Main Folder/01.Script/PlayerScore.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Photon.Pun;
 
-public class PlayerScore : MonoBehaviour
+public class PlayerScore : MonoBehaviourPunCallbacks
 {
+    public float currentScore;
 
-    public float score;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TMP_Text scoreText;
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = currentScore.ToString();
+
+        // 점수 추가
+        if (Input.GetKeyDown(KeyCode.H) && photonView.IsMine)
+        {
+            AddScore(1000);
+        }
+    }
+
+    // 점수 추가 함수
+    [PunRPC] // PunRPC 어트리뷰트로 네트워크에서 호출 가능한 함수로 만듬
+    void AddScoreRPC(float score)
+    {
+        currentScore += score;
+    }
+
+    void AddScore(float score)
+    {
+        // 네트워크를 통해 모든 클라이언트에 점수 업데이트를 전달
+        photonView.RPC("AddScoreRPC", RpcTarget.All, score);
     }
 }
