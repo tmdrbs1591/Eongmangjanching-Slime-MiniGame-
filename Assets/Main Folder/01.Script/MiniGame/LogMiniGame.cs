b82b8b0245b5837b1f8076ea11e2b47fcf477483
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-10-14 18:22:05 +0900

    추가 | 이벤트 스크립트

---
+++ Assets/Main Folder/01.Script/MiniGame/LogMiniGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogMiniGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+
+    void EventStart()
+    {
+
+    }
+}
