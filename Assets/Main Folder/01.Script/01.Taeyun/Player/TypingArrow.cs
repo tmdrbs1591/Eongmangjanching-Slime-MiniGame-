86645a97276127adbe2cc95fc9eb386faec705ba
kimtaeyun <sdh230104@sdh.hs.kr>
2024-10-24 19:16:55 +0900

    추가 | 슬라임 날라감

--- Assets/Main Folder/01.Script/01.Taeyun/Player/TypingArrow.cs
+++ Assets/Main Folder/01.Script/01.Taeyun/Player/TypingArrow.cs
@@ -7,6 +7,19 @@ public class TypingArrow : MonoBehaviourPunCallbacks
 {
     public int currentArrowCount = 0;
     private bool isFailed = false;  // 틀렸는지 여부를 추적할 변수
+    private PlayerScript player;
+    [SerializeField] private float forceAmount;
+
+    private void OnEnable()
+    {
+        player = GetComponentInParent<PlayerScript>();
+        player.rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+    }
+
+    private void OnDisable()
+    {
+        player.rb.constraints = RigidbodyConstraints.None;
+    }
 
     private void Update()
     {
@@ -23,28 +36,36 @@ public class TypingArrow : MonoBehaviourPunCallbacks
                 if (Input.GetKeyDown(KeyCode.UpArrow))
                 {
                     bool isCorrect = ArrowEventManager.instance.CheckArrow("↑", currentArrowCount);
-                    if (!isCorrect) isFailed = true;  // 틀렸으면 isFailed를 true로 설정
+                    if (!isCorrect) FailedArrow();  // 틀렸으면 isFailed를 true로 설정
                     currentArrowCount++;
                 }
                 if (Input.GetKeyDown(KeyCode.DownArrow))
                 {
                     bool isCorrect = ArrowEventManager.instance.CheckArrow("↓", currentArrowCount);
-                    if (!isCorrect) isFailed = true;
+                    if (!isCorrect) FailedArrow();
                     currentArrowCount++;
                 }
                 if (Input.GetKeyDown(KeyCode.LeftArrow))
                 {
                     bool isCorrect = ArrowEventManager.instance.CheckArrow("←", currentArrowCount);
-                    if (!isCorrect) isFailed = true;
+                    if (!isCorrect) FailedArrow();
                     currentArrowCount++;
                 }
                 if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
                     bool isCorrect = ArrowEventManager.instance.CheckArrow("→", currentArrowCount);
-                    if (!isCorrect) isFailed = true;
+                    if (!isCorrect) FailedArrow();
                     currentArrowCount++;
                 }
             }
         }
+    }
+
+    private void FailedArrow()
+    {
+        isFailed = true;
+        player.rb.constraints = RigidbodyConstraints.None;
+
+        player.rb.AddForce((-transform.forward + Vector3.up) * forceAmount, ForceMode.Impulse);
     }
 }
