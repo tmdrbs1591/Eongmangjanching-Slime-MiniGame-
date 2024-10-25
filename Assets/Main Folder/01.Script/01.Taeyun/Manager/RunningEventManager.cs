944ad498ce9be086c145cb5f59793b9b5e1893b0
kimtaeyun <sdh230104@sdh.hs.kr>
2024-10-25 20:49:34 +0900

    추가 | 맵 제작중

---
+++ Assets/Main Folder/01.Script/01.Taeyun/Manager/RunningEventManager.cs
@@ -0,0 +1,51 @@
+// # System
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+// # Unity
+using UnityEngine;
+
+public class RunningEventManager : TimeManager
+{
+    [Header("Props_Log")]
+    [SerializeField] private GameObject log;
+    [SerializeField] private Transform logSpawnPos;
+    [SerializeField] private float logSpawnTime;
+
+    [Header("Props_ball")]
+    [SerializeField] private GameObject ball;
+    [SerializeField] private Transform[] ballSpawnPos;
+    [SerializeField] private float ballSpawnTime;
+
+
+
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(Co_SpawnLog());
+        //StartCoroutine(Co_SpawnBall());
+    }
+
+    IEnumerator Co_SpawnLog()
+    {
+        //PhotonNetwork.Instantiate(log.name, logSpawnPos.position, logSpawnPos.rotation);
+        Instantiate(log, logSpawnPos.position, logSpawnPos.rotation);
+
+        yield return new WaitForSeconds(logSpawnTime);
+
+        yield return StartCoroutine(Co_SpawnLog()); 
+    }
+
+    IEnumerator Co_SpawnBall()
+    {
+        int index = Random.Range(0, ballSpawnPos.Length);
+        
+        Instantiate(ball, ballSpawnPos[index].position, Quaternion.identity);
+
+        yield return new WaitForSeconds(ballSpawnTime);
+
+        yield return StartCoroutine(Co_SpawnBall());
+    }
+}
