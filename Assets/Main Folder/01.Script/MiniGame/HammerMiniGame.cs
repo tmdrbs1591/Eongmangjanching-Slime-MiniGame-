d25275879db6c304aacf7a31508aa71bcf7a5c5c
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-10-25 18:06:43 +0900

    추가 | 플레이어 다 죽으면 겜 종료

--- Assets/Main Folder/01.Script/MiniGame/HammerMiniGame.cs
+++ Assets/Main Folder/01.Script/MiniGame/HammerMiniGame.cs
@@ -25,7 +25,44 @@ public class HammerMiniGame : TimeManager
     {
         // 카운트다운 타이머가 0 이하로 내려가면 숫자 감소
         countdownTimer -= Time.deltaTime;
-            
+
+        // 모든 플레이어의 생사 여부 확인
+        bool allPlayersDead = true; // 모든 플레이어가 죽었는지 확인하는 변수
+
+        foreach (var playerScore in GameManager.instance.playerScores)
+        {
+            // 한 명이라도 살아있는 플레이어가 있으면 false
+            if (!playerScore.isDeath)
+            {
+                allPlayersDead = false;
+                break; // 더 이상 검사할 필요 없음
+            }
+        }
+
+        // 모든 플레이어가 죽었다면 즉시 게임 종료
+        if (allPlayersDead && !isScoreAdded)
+        {
+            foreach (var playerScore in GameManager.instance.playerScores)
+            {
+                // 플레이어가 죽지 않았으면 점수 추가
+                if (!playerScore.isDeath && PhotonNetwork.IsMasterClient)
+                {
+                    Debug.Log("점수 추가");
+                    playerScore.AddScore(1000);  // 점수 추가
+                }
+            }
+
+            // 점수가 추가되었음을 기록
+            isScoreAdded = true;
+
+            GameManager.instance.HammerFalse();
+
+            StartCoroutine(FadeScene());
+
+            // 타이머를 멈추기 위해 함수 종료
+            return;
+        }
+
         if (countdownTimer <= 0)
         {
             // 1초가 지나면 카운트다운 숫자 감소
@@ -37,9 +74,9 @@ public class HammerMiniGame : TimeManager
             // 타이머 초기화
             countdownTimer = 1f;
 
+            // timeRemaining이 0 이하일 때 점수를 추가
             if (timeRemaining <= 0 && !isScoreAdded)
             {
-                // 점수를 한 번만 추가하도록 체크
                 foreach (var playerScore in GameManager.instance.playerScores)
                 {
                     // 플레이어가 죽지 않았으면 점수 추가
@@ -59,4 +96,5 @@ public class HammerMiniGame : TimeManager
             }
         }
     }
+
 }
--- Assets/Main Folder/01.Script/MiniGame/LogMiniGame.cs
+++ Assets/Main Folder/01.Script/MiniGame/LogMiniGame.cs
@@ -59,6 +59,46 @@ public class LogMiniGame : TimeManager
 
     void TimeEnd()
     {
+        // 모든 플레이어가 죽었는지 검사
+        bool allPlayersDead = true; // 모든 플레이어가 죽었음을 나타내는 변수
+
+        foreach (var playerScore in GameManager.instance.playerScores)
+        {
+            // 한 명이라도 죽지 않은 플레이어가 있으면 false
+            if (!playerScore.isDeath)
+            {
+                allPlayersDead = false;
+                break; // 더 이상 검사할 필요 없음
+            }
+        }
+
+        // 모든 플레이어가 죽었다면 즉시 게임 종료
+        if (allPlayersDead)
+        {
+            // 즉시 게임 종료를 위한 처리
+            if (!isScoreAdded) // 점수를 추가하지 않았다면
+            {
+                foreach (var playerScore in GameManager.instance.playerScores)
+                {
+                    // 플레이어가 죽지 않았으면 점수 추가
+                    if (!playerScore.isDeath && PhotonNetwork.IsMasterClient)
+                    {
+                        Debug.Log("점수 추가");
+                        playerScore.AddScore(1000);  // 점수 추가
+                    }
+                }
+
+                // 점수가 추가되었음을 기록
+                isScoreAdded = true;
+
+                // 게임 종료 및 씬 전환 처리
+                StartCoroutine(FadeScene());
+            }
+
+            // 타이머를 1로 설정해도 타이머 감소를 막고, 더 이상의 업데이트는 하지 않음
+            return;
+        }
+
         // 카운트다운 타이머가 0 이하로 내려가면 숫자 감소
         countdownTimer -= Time.deltaTime;
 
@@ -93,4 +133,5 @@ public class LogMiniGame : TimeManager
             }
         }
     }
+
 }
