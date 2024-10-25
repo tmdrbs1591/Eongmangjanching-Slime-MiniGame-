40219fbbf75fafda4fc33eea6ea23fe2475829d0
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-10-25 18:11:02 +0900

    수정 | 뿅망치는 한명만 남았을때

--- Assets/Main Folder/01.Script/MiniGame/HammerMiniGame.cs
+++ Assets/Main Folder/01.Script/MiniGame/HammerMiniGame.cs
@@ -26,25 +26,24 @@ public class HammerMiniGame : TimeManager
         // 카운트다운 타이머가 0 이하로 내려가면 숫자 감소
         countdownTimer -= Time.deltaTime;
 
-        // 모든 플레이어의 생사 여부 확인
-        bool allPlayersDead = true; // 모든 플레이어가 죽었는지 확인하는 변수
+        // 살아있는 플레이어 수를 확인
+        int alivePlayerCount = 0;
 
         foreach (var playerScore in GameManager.instance.playerScores)
         {
-            // 한 명이라도 살아있는 플레이어가 있으면 false
+            // 살아있는 플레이어 수 증가
             if (!playerScore.isDeath)
             {
-                allPlayersDead = false;
-                break; // 더 이상 검사할 필요 없음
+                alivePlayerCount++;
             }
         }
 
-        // 모든 플레이어가 죽었다면 즉시 게임 종료
-        if (allPlayersDead && !isScoreAdded)
+        // 살아있는 플레이어가 1명이라면 게임 종료
+        if (alivePlayerCount == 1 && !isScoreAdded)
         {
             foreach (var playerScore in GameManager.instance.playerScores)
             {
-                // 플레이어가 죽지 않았으면 점수 추가
+                // 점수 추가 (마스터 클라이언트인 경우)
                 if (!playerScore.isDeath && PhotonNetwork.IsMasterClient)
                 {
                     Debug.Log("점수 추가");
@@ -96,5 +95,5 @@ public class HammerMiniGame : TimeManager
             }
         }
     }
-
+    
 }
